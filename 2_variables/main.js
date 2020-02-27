db579dcd4b210380472d70a8a7647b712e12ec1b
Abdullah Barhoum <ka70911@gmail.com>
2020-02-27 22:22:36 +0100

    Add Lesson 2 live code

---
+++ 2_variables/main.js
@@ -0,0 +1,16 @@
+let firstName = "Angela";
+let lastName = "Barhoum";
+//let fullName = firstName + " " + lastName;
+//console.log("My full name is " + fullName);
+
+let fullName = "Abdullah Barhoum";
+let age = 23;
+let cityOfOrigin = "Damascus";
+let canSpeakGerman = true;
+console.log(
+  "My name is " + fullName + " and I am " + age + " yo."
+);
+
+let money = 50;
+let ticketCost = 15;
+console.log("I have " + (money - ticketCost) + "€ left.");
